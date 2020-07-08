32cc26f4f7bd62a78fe7fe374011d5b32da906da
Anudeep D <anudeepd@nbsotech.com>
2020-07-08 10:35:29 +0530

    docker changes

--- server/config.js
+++ server/config.js
@@ -1,11 +1,11 @@
 const dotenv = require("dotenv");
 dotenv.config();
 module.exports = {
-  SNOWFLAKE_URL: "logitech.snowflakecomputing.com",
-  SNOWFLAKE_ACCOUNT: "logitech",
-  SNOWFLAKE_USERNAME: "CUSTOMER_UI_DEV",
-  SNOWFLAKE_PASSWORD: "Cu@tU1D3v",
-  SNOWFLAKE_DB: "POS_DEV",
+  SNOWFLAKE_URL: "",
+  SNOWFLAKE_ACCOUNT: "",
+  SNOWFLAKE_USERNAME: "",
+  SNOWFLAKE_PASSWORD: "",
+  SNOWFLAKE_DB: "abc",
   SNOWFLAKE_SCHEMA: "PRESENTATION",
   v1_base_path: "/api/v1",
   PORT: 3000,
